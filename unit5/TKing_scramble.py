08ee260a2b69ce149ad7b3003dd0898428ddd10c
Tyler King <tylerking@uagateway.org>
2017-02-03 17:14:50 +0000

    Worked o randomizing the letters inside a word

--- unit5/TKing_scramble.py
+++ unit5/TKing_scramble.py
@@ -1,9 +1,23 @@
-word = "I lvoe mselyf"
-word[3] == "o"
-print(word[3])
-word[4] == "v"
-print(word[4])
-print(word)
-# def scramble_word(word):
+import random
+word = "Hello"
+# word[3] == "o"
+# print(word[3])
+# word[4] == "v"
+# print(word[4])
+# print(word)
+def scramble_word(word):
+    word = word[0:5]
+    x = word[1:-1]
+    x = list(x)
+    print(x)
+    random.shuffle(x)
+    print(x)
+    x = str(x)
+    word = x + word[0:5]
+    print(word)
+   
+scramble_word(word)
+    
+    
     
  
