2af90c1ca817476f65a41158eddf5ba55cecac87
Tyler King <tylerking@uagateway.org>
2017-02-06 17:17:14 +0000

    Finished code

--- unit5/TKing_scramble.py
+++ unit5/TKing_scramble.py
@@ -1,23 +1,27 @@
 import random
 word = "Hello"
+word2 = "there"
+word3 = "mister"
 # word[3] == "o"
 # print(word[3])
 # word[4] == "v"
 # print(word[4])
 # print(word)
 def scramble_word(word):
-    word = word[0:5]
-    x = word[1:-1]
-    x = list(x)
-    print(x)
-    random.shuffle(x)
-    print(x)
-    x = str(x)
-    word = x + word[0:5]
-    print(word)
+    Inside_lets = word[1:-1]
+    Inside_lets = list(Inside_lets)
+    random.shuffle(Inside_lets)
+    Combination = [word[0]] + Inside_lets + [word[-1]]
+    word = "".join(Combination)
+    return(word)
    
-scramble_word(word)
+def scramble_phase(word,word2,word3):
+    word = scramble_word(word) 
+    word2 = scramble_word(word2)
+    word3 = scramble_word(word3)
+    scramble_phrase = word + " " + word2 + " " + word3
+    print (scramble_phrase)
+scramble_phase(word,word2,word3)
     
     
     
- 
