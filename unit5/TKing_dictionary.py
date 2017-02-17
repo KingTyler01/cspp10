009a1b86f4a70807267690682cf47319a21b1fe7
Tyler King <tylerking@uagateway.org>
2017-02-17 17:16:14 +0000

    Worked on and completed all functions that get the users input

--- unit5/TKing_dictionary.py
+++ unit5/TKing_dictionary.py
@@ -1,35 +1,38 @@
 from pprint import pprint
-# d = {
-#     "name":"King",
-#     "birthyear":"Decemeber",
-#     "grade":10,
-#     "A period": "Chemistry",
-#     "B period": "Algebra II",
-#     "F/Grey" : "Weight Room",
-#     "F/Red": "Health",
-#     "C period" : "English",
-#     "D period": "Global",
-#     "E period": "Global"
-# }
-# for key in d:
-#     print(d[key])
-# pprint(d[input("Schedule period: ")])
-
-def letsdoit():
-    d = {}
-    choose = input("[Add]key-value\n[Remove]key-value\n[Update]key-value\n[Exit]\n:")
-    if choose == "add" or choose == "Add":
-        d[input("What new key would you like to add to the dictionary?: ")] = input("What is the value of this key?: ")
+choice = input("[Add]key-value\n[Remove]key-value\n[Update]key-value\n[Exit]\n:")
+d = {}
+def add(choice,d):
+    if choice == "add" or choice == "Add":
+        check = input("What new key would you like to add to the dictionary?: ")
+        if check in d:
+            print("Invalid. Input a new key into the dictionary: ")
+            print(check)
+        else:
+            d[check] = input("What is the value of this key?: ")
+            pprint(d)
+            
+def remove(choice,d):
+    if choice == "remove" or choice =="Remove":
+        remove = input("What new key would you like to remove from the dictionary?: ")
+        del d[remove]
         pprint(d)
-        # add = str(input("What key would you like to add?: "))
-        # add == add
-        # result_add = str(input("What is the value of this key: "))
-        # d[add] = result_add
-        # pprint(d)
-        
-        
+
+def update(choice,d ):
+    if choice == "Update" or choice == "update":
+        check = input ("What key do you want to add to the list?: ")
+        value = input ("What value do you want the key to have?: ")
    
-    # pprint(d)
-    # pprint(d[input("Schedule : ")])
+    if choice not in d:
+        d[check] = value
+        pprint(d)
+        
+def Exit(choice,d):
+    if choice == "Exit" or "exit":
+        print("The End")
+
+        
+        
     
-letsdoit()
+        
+
+Exit(choice,d)
